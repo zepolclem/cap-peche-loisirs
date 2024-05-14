76804938fad086e14272522aacd85d650867652e
zepolclem <lopez.clmnt@gmail.com>
2024-05-14 23:29:48 +0200

    add directus sdk,  prestations data and images, view transitions

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly PUBLIC_DIRECTUS_BASE_URL: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
---
+++ src/utils/get-directus-client.ts
@@ -0,0 +1,24 @@
+import { createDirectus, rest, readItems } from '@directus/sdk';
+
+const client = createDirectus(import.meta.env.PUBLIC_DIRECTUS_BASE_URL).with(rest());
+
+
+export const getDirectusItems = async (item: string) => {
+    const result = await client.request(
+        readItems(item, {
+            fields: ['*'],
+        })
+    );    
+
+    return result;
+}
+
+export const getDirectusItem = async (item: string) => {
+    const result = await client.request(
+        readItems(item, {
+            fields: ['*'],
+        })
+    );    
+
+    return result;
+}
