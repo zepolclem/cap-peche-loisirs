bfc826c81838796e1c6bcfe273aa989b1cfc9171
zepolclem <lopez.clmnt@gmail.com>
2024-05-24 23:22:42 +0200

    add astronav gg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  integrations: [tailwind()],
   image: {
     domains: ["directus.cappecheloisirs.com"],
     remotePatterns: [{
