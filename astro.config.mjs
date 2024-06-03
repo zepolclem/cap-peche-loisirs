464862c91373ab38342c05ef2ae544dea3669bd1
zepolclem <lopez.clmnt@gmail.com>
2024-06-03 22:49:03 +0200

    retry alpine js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
   image: {
     domains: ["directus.cappecheloisirs.com"],
     remotePatterns: [{
       protocol: "https"
     }]
-  },
+  }
 });
